f68e12bf89fce9a9d14d6d8c91657133cfab6d04
Mauricio Poveda <mauricio@poveda.co.cr>
2015-08-06 16:21:49 -0600

    ...

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,21 +9,15 @@
   function MainController(MainTiles) {
     var mc = this;
     mc.tiles = [];
-    mc.loaded = false;
-    mc.modalShown = false;
-
+    mc.loaded = false;  
+ 
     // main tiles service call
     MainTiles.tiles().success(function(data) {
       mc.tiles = data;
       mc.loaded = true;
     });
 
-    
-    // modal controls
-    mc.toggleModal = function() {
-      console.log('toggle modal!');
-      mc.modalShown = !mc.modalShown;
-    };
+
 
 
   }
